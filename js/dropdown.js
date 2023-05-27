1c33978dd615d5c0524ee1ff101654f6ec9f2e25
Rasif Taghiyev <rasiftagizade11@gmail.com>
2023-05-28 01:05:46 +0400

    Dashboard navbar menu

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,8 +1,20 @@
-const list = document.querySelectorAll(".folder-explorer-list>li")
-console.log(list)
-list.forEach((item) => {
-    const li = item.children[1];
-    item.addEventListener("click",()=>{
-        li.classList.toggle("list-active")
+//* Dropdown menu
+const dropdowns = document.querySelectorAll(".dshb-dropdown-menu");
+console.log(dropdowns);
+dropdowns.forEach(menu => {
+    menu.addEventListener("click", () => {
+        const selectMenu = menu.querySelector(".menu-select"),
+            selectMenuText = selectMenu.querySelector("p"),
+            optionsMenu = menu.querySelectorAll(".menu-option-content");
+        //? Select Menu add class
+        selectMenu.classList.toggle("dropdown-menu-open");
+
+        optionsMenu.forEach(option => {
+            option.addEventListener("click", () => {
+                const menuTextContent = option.querySelector("p");
+                selectMenuText.textContent = menuTextContent.textContent;
+            })
+        })
+
     })
 })
