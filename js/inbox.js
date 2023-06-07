72de8f5299789b19991815260856db9611d82085
Rasif Taghiyev <rasiftagizade11@gmail.com>
2023-06-08 00:29:59 +0400

    inbox page completed

--- js/inbox.js
+++ js/inbox.js
@@ -1,7 +1,15 @@
+//! Inbox btn active class:
+const inboxBtns = document.querySelectorAll('.inbox-sidebar-filter-btns > button');
 const btn = document.querySelector('.filter-btn-active');
-function leftClick(e) {
+inboxBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        inboxBtns.forEach(btn => btn.classList.remove('btn-active'))
+        btn.classList.add('btn-active');
+    })
+})
+
+function leftClick() {
     btn.style.left = "5px";
-    
 }
 function rightClick() {
     btn.style.left = "180px";
@@ -13,8 +21,17 @@ const users = document.querySelectorAll('.inbox-sidebar-user');
 
 users.forEach(user => {
     user.addEventListener('click', () => {
-        users.forEach(user => user.classList.remove('user-active'))
+        users.forEach(user => user.classList.remove('usxer-active'))
         user.classList.add('user-active');
     })
 })
 
+
+//! Contact Message active class:
+let contactMsg = document.querySelector('.contact-message');
+let msgSelected = contactMsg.querySelector('.contact-message-select');
+msgSelected.addEventListener('click', () => {
+    contactMsg.classList.toggle('contact-active');
+
+    msgSelected.classList.toggle('active');
+})
