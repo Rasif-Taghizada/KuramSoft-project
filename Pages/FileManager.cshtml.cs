730c4e86183b3d9fa7afd0a1d728388fea777cb1
akif.cicek@kuramsoft.com <akif.cicek@kuramsoft.com>
2023-07-09 12:35:14 +0300

    File manager ve kanban dinamik modele alındı

---
+++ Pages/FileManager.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class FileManagerModel : PageModel
+    {
+        private readonly ILogger<FileManagerModel> _logger;
+
+        public FileManagerModel(ILogger<FileManagerModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Pages/Kanban.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class KanbanModel : PageModel
+    {
+        private readonly ILogger<KanbanModel> _logger;
+
+        public KanbanModel(ILogger<KanbanModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
