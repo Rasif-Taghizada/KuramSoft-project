91cd7f5b84ba7fb6c257ab4bb6f52f6bfe964917
Rasif Taghiyev <rasiftagizade11@gmail.com>
2023-07-11 15:12:47 +0400

    C# html pages debug

---
+++ Pages/Form.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class FormModel : PageModel
+    {
+        private readonly ILogger<FormModel> _logger;
+
+        public FormModel(ILogger<FormModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- wwwroot/js/kanban.js
+++ wwwroot/js/kanban.js
@@ -11,7 +11,7 @@ userImages.forEach((user, index) => {
 //! All List Cards Scroll bar;
 const listCards = document.querySelectorAll(".kanban-list-cards");
 // console.log(listCards);
-listCards.forEach(card => {
+listCards?.forEach(card => {
     // console.log(card.scrollHeight);
     if (card.scrollHeight > 500) {
         card.style.overflowY = "scroll";
@@ -20,3 +20,5 @@ listCards.forEach(card => {
         card.style.overflowY = "auto";
     }
 });
+
+console.log("Hello World")
