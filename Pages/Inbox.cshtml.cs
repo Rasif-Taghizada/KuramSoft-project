99b01e3aef38990d457cf05f6b52cbbda9d51077
Rasif Taghiyev <rasiftagizade11@gmail.com>
2023-07-18 21:22:44 +0400

    Latest version

---
+++ Pages/Inbox.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class InboxModel : PageModel
+    {
+        private readonly ILogger<InboxModel> _logger;
+
+        public InboxModel(ILogger<InboxModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
